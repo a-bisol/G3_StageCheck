43fa4c72b3ea49dc53fec4c9656edc2b9ef7232a
Ash Bisol <contact@asheng.ca>
2026-06-03 22:15:12 -0400

    Added icons to NavBar and updated selection tab logic

--- app/src/main/java/mads/group3/stagecheck/common/components/NavBar.kt
+++ app/src/main/java/mads/group3/stagecheck/common/components/NavBar.kt
@@ -1,10 +1,24 @@
 package mads.group3.stagecheck.common.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Map
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import mads.group3.stagecheck.navigation.Screens
 
 /* TODO
@@ -13,18 +27,24 @@ import mads.group3.stagecheck.navigation.Screens
 */
 @Composable
 fun NavBar(navController: NavController) {
-    val items = listOf(
-        Screens.Main.Dash,
-        Screens.Main.Search,
-        Screens.Main.Map,
-        Screens.Main.Favourites,
-        Screens.Main.Profile
-    )
+    val items = remember {
+        listOf(
+            Screens.Main.Dash,
+            Screens.Main.Search,
+            Screens.Main.Map,
+            Screens.Main.Favourites,
+            Screens.Main.Profile
+        )
+    }
+
+    val currentDestination = navController.currentBackStackEntryAsState().value
+    val currentRoute = currentDestination?.destination?.route
 
     NavigationBar {
         items.forEach { screen ->
+            val isSelected = currentRoute == screen.route
             NavigationBarItem(
-                selected = navController.currentDestination?.route == screen.route,
+                selected = isSelected,
                 onClick = {
                     navController.navigate(screen.route) {
                         popUpTo(navController.graph.startDestinationId) {
@@ -34,9 +54,25 @@ fun NavBar(navController: NavController) {
                         restoreState = true
                     }
                 },
-                icon = {},
-                label = { Text(screen.subRoute) }
+                icon = {
+                    Icon(
+                        imageVector = getIconForScreen(screen, isSelected),
+                        contentDescription = screen.subRoute
+                    )
+                },
+                label = null
             )
         }
+    }
+}
+
+@Composable
+fun getIconForScreen(screen: Screens.Main, isSelected: Boolean): ImageVector {
+    return when (screen) {
+        Screens.Main.Dash -> if (isSelected) Icons.Filled.Home else Icons.Outlined.Home
+        Screens.Main.Search -> if (isSelected) Icons.Filled.Search else Icons.Outlined.Search
+        Screens.Main.Map -> if (isSelected) Icons.Filled.Map else Icons.Outlined.Map
+        Screens.Main.Favourites -> if (isSelected) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder
+        Screens.Main.Profile -> if (isSelected) Icons.Filled.Person else Icons.Outlined.Person
     }
 }
