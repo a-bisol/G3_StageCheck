fba7bd3e4f0053e1ebeb06173ade02ec6b3f6ff7
Ash Bisol <contact@asheng.ca>
2026-06-04 13:17:10 -0400

    Added multiple placeholder ReusableEventCards

--- app/src/main/java/mads/group3/stagecheck/ui/screens/DashScreen.kt
+++ app/src/main/java/mads/group3/stagecheck/ui/screens/DashScreen.kt
@@ -1,27 +1,35 @@
 package mads.group3.stagecheck.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import mads.group3.stagecheck.common.components.ReusableEventCard
 import mads.group3.stagecheck.navigation.Screens
 
 @Composable
 fun DashScreen(navController: NavController) {
-    Column {
-        Text("Placeholder Dash")
-        Button(onClick = {
-            navController.navigate(Screens.DetailEvent.passEventId(10))
-        }) {
-            Text("Go to Event 10")
-        }
+    Column(
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
         ReusableEventCard(
             imageUrl = null,
             headliner = "Emo Nite",
             venue = "Sneaky Dees",
-            onClick = { /* Nothing ^-^ */ }
+            onClick = { navController.navigate(Screens.DetailEvent.passEventId("10")) }
+        )
+        ReusableEventCard(
+            imageUrl = null,
+            headliner = "Prince Daddy & The Hyena",
+            venue = "Axis Club",
+            onClick = { navController.navigate(Screens.DetailEvent.passEventId("12")) }
+        )
+        ReusableEventCard(
+            imageUrl = null,
+            headliner = "Emo Nite",
+            venue = "Sneaky Dees",
+            onClick = { navController.navigate(Screens.DetailEvent.passEventId("10_again")) }
         )
     }
 }
